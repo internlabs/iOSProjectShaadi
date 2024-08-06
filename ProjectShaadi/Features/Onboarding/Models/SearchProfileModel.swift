8f632e24707967e85db137764b0f425f643d2862
Rohit Das <das2001rohit@gmail.com>
2024-08-06 16:37:31 +0530

    SearchProfileID Screen

---
+++ ProjectShaadi/Features/Onboarding/Models/SearchProfileModel.swift
@@ -0,0 +1,16 @@
+//
+//  SearchProfileModel.swift
+//  ProjectShaadi
+//
+//  Created by ROHIT DAS on 17/07/24.
+//
+
+import Foundation
+
+struct SearchProfileModel {
+    let title: String
+    let placeholder: String
+    let searchByProfileButtonText: String
+    let searchByCriteriaButtonText: String
+    
+}
---
+++ ProjectShaadi/Features/Onboarding/Views/CustomViews/SearchProfileView.swift
@@ -0,0 +1,104 @@
+//
+//  SearchProfileView.swift
+//  ProjectShaadi
+//
+//  Created by ROHIT DAS on 17/07/24.
+//
+
+import UIKit
+
+class SearchProfileView: UIView {
+     let titleLabel = UILabel()
+     let profileIDTextField = UITextField()
+     let showProfilesButton = UIButton(type: .system)
+     let orLabel = UILabel()
+     let searchByCriteriaButton = UIButton(type: .system)
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupViews() {
+        backgroundColor = .white
+        
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        profileIDTextField.translatesAutoresizingMaskIntoConstraints = false
+        showProfilesButton.translatesAutoresizingMaskIntoConstraints = false
+        orLabel.translatesAutoresizingMaskIntoConstraints = false
+        searchByCriteriaButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 24)
+        titleLabel.textColor = .black
+        
+        setupTextField(profileIDTextField, placeholder: "")
+        
+        showProfilesButton.backgroundColor = UIColor(hex: "#EF4B4B")
+        showProfilesButton.setTitleColor(.white, for: .normal)
+        showProfilesButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        showProfilesButton.layer.cornerRadius = 20
+        showProfilesButton.clipsToBounds = true
+       
+        orLabel.font = UIFont.systemFont(ofSize: 14)
+        orLabel.text = "OR"
+        orLabel.textColor = .gray
+        
+        searchByCriteriaButton.backgroundColor = UIColor(hex:"#F4F4F4")
+        searchByCriteriaButton.setTitleColor(UIColor(hex: "#EF4B4B"), for: .normal)
+        searchByCriteriaButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        searchByCriteriaButton.layer.cornerRadius = 20
+        searchByCriteriaButton.clipsToBounds = true
+        
+        
+        addSubview(titleLabel)
+        addSubview(profileIDTextField)
+        addSubview(showProfilesButton)
+        addSubview(orLabel)
+        addSubview(searchByCriteriaButton)
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor,constant: 40),
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 46),
+            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -25),
+            
+            profileIDTextField.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 30),
+            profileIDTextField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 46),
+            profileIDTextField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -46),
+            profileIDTextField.heightAnchor.constraint(equalToConstant: 50),
+            
+            showProfilesButton.heightAnchor.constraint(equalToConstant: 55),
+            showProfilesButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 50),
+            showProfilesButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -50),
+            
+            orLabel.topAnchor.constraint(equalTo: showProfilesButton.bottomAnchor, constant: 20),
+            orLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            
+            searchByCriteriaButton.topAnchor.constraint(equalTo: orLabel.bottomAnchor, constant: 20),
+            searchByCriteriaButton.heightAnchor.constraint(equalToConstant: 55),
+            searchByCriteriaButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 50),
+            searchByCriteriaButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -50),
+            searchByCriteriaButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -80)
+        ])
+    }
+    
+    func configure(with item: SearchProfileModel){
+        titleLabel.text = item.title
+        setupTextField(profileIDTextField, placeholder: item.placeholder)
+        showProfilesButton.setTitle(item.searchByProfileButtonText, for: .normal)
+        searchByCriteriaButton.setTitle(item.searchByCriteriaButtonText, for: .normal)
+    }
+
+    private func setupTextField(_ textField: UITextField, placeholder: String){
+        textField.placeholder = placeholder
+        textField.borderStyle = .roundedRect
+        textField.setPlaceholder(color: UIColor(hex:"#000000"))
+        textField.backgroundColor = UIColor(hex: "#F4F4F4")
+        textField.textColor = .black
+    }
+    
+}
