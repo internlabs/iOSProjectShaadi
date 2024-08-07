b3183265cf44e5aa31a068b94310f9989bc8fe0d
vvaditya23 <vvaditya23@icloud.com>
2024-08-07 15:36:19 +0530

    ⛏️  Address PR comments (PR #1)

--- iOSProjectShaadi/Onboarding/Login/LoginViewController.swift
+++ iOSProjectShaadi/Onboarding/Login/LoginViewController.swift
@@ -7,13 +7,64 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+final class LoginViewController: UIViewController {
     // UI elements
-    let titleLabel = UILabel()
-    let subtitleLabel = UILabel()
-    let numberTextField = UITextField()
-    let otpButton = UIButton()
-    let passwordButton = UIButton()
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Welcome Back 👻"
+        label.font = UIFont(name: "KumbhSans-SemiBold", size: 24)
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(label)
+        return label
+    }()
+    
+    lazy var subtitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Login to your Account"
+        label.font = UIFont(name: "KumbhSans-Regular", size: 20)
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(label)
+        return label
+    }()
+    
+    lazy var numberTextField: UITextField = {
+        let textField = UITextField()
+        textField.backgroundColor = UIColor(red: 244/255, green: 244/255, blue: 244/255, alpha: 1)
+        textField.font = UIFont(name: "KumbhSans-Medium", size: 14)
+        textField.placeholder = "+91 Enter Phone Number"
+        textField.layer.cornerRadius = 5.0
+        
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(textField)
+        return textField
+    }()
+    
+    lazy var otpButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Send One Time Password", for: .normal)
+        button.backgroundColor = UIColor(red: 239/255, green: 75/255, blue: 75/255, alpha: 1)
+        button.titleLabel?.font = UIFont(name: "KumbhSans-SemiBold", size: 16)
+        button.layer.cornerRadius = 20
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(button)
+        return button
+    }()
+    
+    lazy var passwordButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Login using Password", for: .normal)
+        button.backgroundColor = UIColor(red: 244/255, green: 244/255, blue: 244/255, alpha: 1)
+        button.titleLabel?.font = UIFont(name: "KumbhSans-SemiBold", size: 16)
+        button.layer.cornerRadius = 20
+        button.setTitleColor(UIColor(red: 239/255, green: 75/255, blue: 75/255, alpha: 1), for: .normal)
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(button)
+        return button
+    }()
     
     // Helper components
     let viewModel = LoginViewModel()
@@ -21,70 +72,43 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        setupUIelements()
+                
+        otpButton.addTarget(self, action: #selector(otpButtonTapped), for: .touchUpInside)
+        passwordButton.addTarget(self, action: #selector(passwordButtonTapped), for: .touchUpInside)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        setupConstraints()
     }
 }
 
+// MARK: UI elements
 extension LoginViewController {
-    private func setupUIelements() {
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        subtitleLabel.translatesAutoresizingMaskIntoConstraints = false
-        numberTextField.translatesAutoresizingMaskIntoConstraints = false
-        otpButton.translatesAutoresizingMaskIntoConstraints = false
-        passwordButton.translatesAutoresizingMaskIntoConstraints = false
-
-        view.addSubview(titleLabel)
-        view.addSubview(subtitleLabel)
-        view.addSubview(numberTextField)
-        view.addSubview(otpButton)
-        view.addSubview(passwordButton)
-
+    private func setupConstraints() {
         NSLayoutConstraint.activate([
-            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 148),
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
             titleLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 46),
+            titleLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -46),
             
-            subtitleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 202),
+            subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10),
             subtitleLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 46),
+            subtitleLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -46),
             
-            numberTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 280),
+            numberTextField.topAnchor.constraint(equalTo: subtitleLabel.bottomAnchor, constant: 30),
             numberTextField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 46),
             numberTextField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -46),
             numberTextField.heightAnchor.constraint(equalToConstant: 50),
             
-            otpButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 378),
+            otpButton.topAnchor.constraint(equalTo: numberTextField.bottomAnchor, constant: 48),
             otpButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 51),
             otpButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -51),
             otpButton.heightAnchor.constraint(equalToConstant: 55),
             
-            passwordButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 454),
+            passwordButton.topAnchor.constraint(equalTo: otpButton.bottomAnchor, constant: 26),
             passwordButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 51),
             passwordButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -51),
             passwordButton.heightAnchor.constraint(equalToConstant: 55)
-            ])
-        
-        titleLabel.text = "Welcome Back 👻"
-        titleLabel.font = UIFont(name: "KumbhSans-SemiBold", size: 24)
-        
-        subtitleLabel.text = "Login to your Account"
-        subtitleLabel.font = UIFont(name: "KumbhSans-Regular", size: 20)
-        
-        numberTextField.backgroundColor = UIColor(red: 244/255, green: 244/255, blue: 244/255, alpha: 1)
-        numberTextField.font = UIFont(name: "KumbhSans-Medium", size: 14)
-        numberTextField.placeholder = "+91 Enter Phone Number"
-        numberTextField.layer.cornerRadius = 5.0
-        
-        otpButton.setTitle("Send One Time Password", for: .normal)
-        otpButton.backgroundColor = UIColor(red: 239/255, green: 75/255, blue: 75/255, alpha: 1)
-        otpButton.titleLabel?.font = UIFont(name: "KumbhSans-SemiBold", size: 16)
-        otpButton.layer.cornerRadius = 20
-        otpButton.addTarget(self, action: #selector(otpButtonTapped), for: .touchUpInside)
-        
-        passwordButton.setTitle("Login using Password", for: .normal)
-        passwordButton.backgroundColor = UIColor(red: 244/255, green: 244/255, blue: 244/255, alpha: 1)
-        passwordButton.titleLabel?.font = UIFont(name: "KumbhSans-SemiBold", size: 16)
-        passwordButton.layer.cornerRadius = 20
-        passwordButton.setTitleColor(UIColor(red: 239/255, green: 75/255, blue: 75/255, alpha: 1), for: .normal)
-        passwordButton.addTarget(self, action: #selector(passwordButtonTapped), for: .touchUpInside)
+        ])
     }
 }
 
--- iOSProjectShaadi/Onboarding/Login/OTPViewController.swift
+++ iOSProjectShaadi/Onboarding/Login/OTPViewController.swift
@@ -7,78 +7,105 @@
 
 import UIKit
 
-class OTPViewController: UIViewController {
-    let titleLabel = UILabel()
-    let subtitleLabel = UILabel()
-    var otpTextFields = [UITextField]()
-    let resendButton = UIButton()
-    let submitButton = UIButton()
+final class OTPViewController: UIViewController {
+    // UI elements
+    lazy var titleLabel: UILabel = {
+        lazy var label = UILabel()
+        label.numberOfLines = 0
+        label.text = "Enter the code sent on your number 📱"
+        label.font = UIFont(name: "KumbhSans-SemiBold", size: 24)
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(label)
+        return label
+    }()
+    
+    lazy var subtitleLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        let subtitleText = "Enter 4-digit code we have sent to the phone number +91 7977828994"
+        let subtitleAttributedText = NSMutableAttributedString(string: subtitleText)
+        let phoneRange = (subtitleText as NSString).range(of: "+91 7977828994")
+        let subtitleTextFont = UIFont(name: "KumbhSans-Regular", size: 16)
+        let subtitleNumberFont = UIFont(name: "KumbhSans-SemiBold", size: 16)
+        subtitleAttributedText.addAttribute(.font, value: subtitleTextFont as Any, range: NSMakeRange(0, subtitleText.count))
+        subtitleAttributedText.addAttribute(.font, value: subtitleNumberFont as Any, range: phoneRange)
+        label.attributedText = subtitleAttributedText
+        label.textAlignment = .center
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(label)
+        return label
+    }()
+        
+    lazy var resendButton: UIButton = {
+        let button = UIButton()
+        let resendButtonText = "Didn’t get the OTP? Resend Again"
+        let resendButtonAttributedText = NSMutableAttributedString(string: resendButtonText)
+        let resendTextRange = (resendButtonText as NSString).range(of: "Resend Again")
+        let resendButtonTextFont = UIFont(name: "KumbhSans-Regular", size: 16)
+        let resendAgainTextFont = UIFont(name: "KumbhSans-SemiBold", size: 16)
+        resendButtonAttributedText.addAttribute(.font, value: resendButtonTextFont as Any, range: NSMakeRange(0, resendButtonText.count))
+        resendButtonAttributedText.addAttribute(.font, value: resendAgainTextFont as Any, range: resendTextRange)
+        resendButtonAttributedText.addAttribute(.foregroundColor, value: UIColor(red: 221/255, green: 87/255, blue: 82/255, alpha: 1), range: resendTextRange)
+        resendButtonAttributedText.addAttribute(.underlineStyle, value: NSUnderlineStyle.thick.rawValue, range: resendTextRange)
+        resendButtonAttributedText.addAttribute(.underlineColor, value: UIColor(red: 221/255, green: 87/255, blue: 82/255, alpha: 1), range: resendTextRange)
+        button.setAttributedTitle(resendButtonAttributedText, for: .normal)
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(button)
+        return button
+    }()
+    
+    lazy var submitButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Next", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont(name: "KumbhSans-SemiBold", size: 16)
+        button.layer.cornerRadius = 20
+        button.backgroundColor = UIColor(red: 221/255, green: 87/255, blue: 82/255, alpha: 1)
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(button)
+        return button
+    }()
+    
+    let otpStackView = OTPStackView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        setupUIelements()
+            
+        otpStackView.delegate = self
+        otpStackView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(otpStackView)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        setupConstraints()
     }
 }
 
 // MARK: UI elements
 extension OTPViewController {
-    private func setupUIelements() {
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        subtitleLabel.translatesAutoresizingMaskIntoConstraints = false
-        resendButton.translatesAutoresizingMaskIntoConstraints = false
-        submitButton.translatesAutoresizingMaskIntoConstraints = false
-        
-        view.addSubview(titleLabel)
-        view.addSubview(subtitleLabel)
-        view.addSubview(resendButton)
-        view.addSubview(submitButton)
-        
-        let otpStackView = UIStackView()
-        otpStackView.axis = .horizontal
-        otpStackView.alignment = .fill
-        otpStackView.distribution = .fillEqually
-        otpStackView.spacing = 10
-        
-        for textFieldNumber in 0..<4 {
-            let textField = UITextField()
-            textField.tag = textFieldNumber
-            textField.layer.borderWidth = 1
-            textField.layer.borderColor = UIColor.black.cgColor
-            textField.layer.cornerRadius = 27
-            textField.clipsToBounds = true
-            textField.textAlignment = .center
-            textField.keyboardType = .numberPad
-            textField.delegate = self
-            textField.font = UIFont.systemFont(ofSize: 24)
-            textField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
-            otpTextFields.append(textField)
-            otpStackView.addArrangedSubview(textField)
-            
-            textField.heightAnchor.constraint(equalToConstant: 60).isActive = true
-            textField.widthAnchor.constraint(equalToConstant: 60).isActive = true
-        }
-        
-        otpStackView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(otpStackView)
-        
+    private func setupConstraints() {
         NSLayoutConstraint.activate([
-            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 124),
-            titleLabel.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 63),
-            titleLabel.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -63),
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 63),
+            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -63),
             titleLabel.heightAnchor.constraint(equalToConstant: 62),
             
-            subtitleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 247),
-            subtitleLabel.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 55),
-            subtitleLabel.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -55),
+            subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 60),
+            subtitleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 55),
+            subtitleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -55),
             subtitleLabel.heightAnchor.constraint(equalToConstant: 60),
             
-            otpStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 348),
-            otpStackView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 68),
-            otpStackView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -68),
+            otpStackView.topAnchor.constraint(equalTo: subtitleLabel.bottomAnchor, constant: 40),
+            otpStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 68),
+            otpStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -68),
             otpStackView.heightAnchor.constraint(equalToConstant: 60),
             
-            resendButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 455),
+            resendButton.topAnchor.constraint(equalTo: otpStackView.bottomAnchor, constant: 40),
             resendButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             
             submitButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
@@ -86,60 +113,9 @@ extension OTPViewController {
             submitButton.heightAnchor.constraint(equalToConstant: 55),
             submitButton.widthAnchor.constraint(equalToConstant: 161)
         ])
-        
-        titleLabel.numberOfLines = 0
-        subtitleLabel.numberOfLines = 0
-        
-        titleLabel.text = "Enter the code sent on your number 📱"
-        titleLabel.font = UIFont(name: "KumbhSans-SemiBold", size: 24)
-        
-        let subtitleText = "Enter 4-digit code we have sent to the phone number +91 7977828994"
-        let subtitleAttributedText = NSMutableAttributedString(string: subtitleText)
-        let phoneRange = (subtitleText as NSString).range(of: "+91 7977828994")
-        let subtitleTextFont = UIFont(name: "KumbhSans-Regular", size: 16)
-        let subtitleNumberFont = UIFont(name: "KumbhSans-SemiBold", size: 16)
-        subtitleAttributedText.addAttribute(.font, value: subtitleTextFont, range: NSMakeRange(0, subtitleText.count))
-        subtitleAttributedText.addAttribute(.font, value: subtitleNumberFont, range: phoneRange)
-        subtitleLabel.attributedText = subtitleAttributedText
-        subtitleLabel.textAlignment = .center
-        
-        let resendButtonText = "Didn’t get the OTP? Resend Again"
-        let resendButtonAttributedText = NSMutableAttributedString(string: resendButtonText)
-        let resendTextRange = (resendButtonText as NSString).range(of: "Resend Again")
-        let resendButtonTextFont = UIFont(name: "KumbhSans-Regular", size: 16)
-        let resendAgainTextFont = UIFont(name: "KumbhSans-SemiBold", size: 16)
-        resendButtonAttributedText.addAttribute(.font, value: resendButtonTextFont, range: NSMakeRange(0, resendButtonText.count))
-        resendButtonAttributedText.addAttribute(.font, value: resendAgainTextFont, range: resendTextRange)
-        resendButtonAttributedText.addAttribute(.foregroundColor, value: UIColor(red: 221/255, green: 87/255, blue: 82/255, alpha: 1), range: resendTextRange)
-        resendButtonAttributedText.addAttribute(.underlineStyle, value: NSUnderlineStyle.thick.rawValue, range: resendTextRange)
-        resendButtonAttributedText.addAttribute(.underlineColor, value: UIColor(red: 221/255, green: 87/255, blue: 82/255, alpha: 1), range: resendTextRange)
-
-        resendButton.setAttributedTitle(resendButtonAttributedText, for: .normal)
-        
-        submitButton.setTitle("Next", for: .normal)
-        submitButton.setTitleColor(.white, for: .normal)
-        submitButton.titleLabel?.font = UIFont(name: "KumbhSans-SemiBold", size: 16)
-        submitButton.layer.cornerRadius = 20
-        submitButton.backgroundColor = UIColor(red: 221/255, green: 87/255, blue: 82/255, alpha: 1)
     }
 }
 
-// MARK: Delegate conformance
-extension OTPViewController: UITextFieldDelegate {
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        textField.layer.borderColor = UIColor(red: 221/255, green: 87/255, blue: 82/255, alpha: 1).cgColor
-    }
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        textField.layer.borderColor = UIColor.label.cgColor
-    }
-    
-    @objc func textFieldDidChange(_ textField: UITextField) {
-        if let text = textField.text, text.count >= 1 {
-            if let nextField = view.viewWithTag(textField.tag + 1) as? UITextField {
-                nextField.becomeFirstResponder()
-            } else {
-                textField.resignFirstResponder()
-            }
-        }
-    }
+extension OTPViewController: OTPDelegate {
+    func didChangeValidity(isValid: Bool) {}
 }
